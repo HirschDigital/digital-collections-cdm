52e54e362ec49916407791fb98e491283881aec8
HirschDigital <slong@mfah.org>
2024-09-09 16:37:55 -0500

    Update collapse.js

--- js/collapse.js
+++ js/collapse.js
@@ -24,18 +24,3 @@ document.addEventListener('cdm-collection-landing-page:update', accordionCollaps
 document.addEventListener('cdm-custom-page:ready', accordionCollapse);
 document.addEventListener('cdm-custom-page:update', accordionCollapse);
 })();
-
-
-
-
-function(t) {
-  "A" === t.currentTarget.tagName && t.preventDefault();
-  var n = st(this),
-    e = Fn.getSelectorFromElement(this),
-    i = [].slice.call(document.querySelectorAll(e));
-  st(i).each(function() {
-    var t = st(this),
-      e = t.data(lt) ? "toggle" : n.data();
-    Tt._jQueryInterface.call(t, e)
-  })
-}
