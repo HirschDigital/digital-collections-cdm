c3cdd372a8b1e3cda6ac8e5118f60d564edd0e8b
HirschDigital <slong@mfah.org>
2025-06-25 13:37:09 -0500

    update reformatter

--- js/link-reformatter-thumb+artist.js
+++ js/link-reformatter-thumb+artist.js
@@ -1,79 +1,91 @@
 (function () {
   'use strict';
-function reformatField(nickname,syntax) {
-  let fieldsToChange = document.querySelectorAll('tr.ItemMetadata-metadatarow.field-' + nickname + ' > td.field-value > span');
-  if (fieldsToChange) {
-    fieldsToChange.forEach(function(field){
-      var originalText = field.textContent;
-      let displayText = '', linkTarget = '', linkThumb = '', origTextArray = [];
-      switch (syntax) {
-        case 'markdown':
-          origTextArray = originalText.split(';');
-          if (origTextArray) {
-            field.innerHTML = '';
-            origTextArray.forEach(function(segment){
-              let eachLink = document.createElement('a');
-              let imgBox = document.createElement('div');
-              let imgElement = document.createElement('img');
-              let capArtist = document.createElement('h3');
-              let capText = document.createElement('h3');
-              let textContainer = document.createElement('div');
+
+  function reformatField(nickname, syntax, target) {
+    let fieldsToChange = document.querySelectorAll(
+      'tr.ItemMetadata-metadatarow.field-' + nickname + ' > td.field-value > span'
+    );
+    if (fieldsToChange.length > 0) {
+      fieldsToChange.forEach(function (field) {
+        const originalText = field.textContent.trim();
+        let displayText = '',
+          linkTarget = '',
+          linkThumb = '',
+          origTextArray = [];
+
+        if (!originalText.includes('[') || !originalText.includes('(')) return;
+
+        switch (syntax) {
+          case 'markdown':
+            origTextArray = originalText.split(';');
+            field.innerHTML = ''; // Clear original text
+
+            origTextArray.forEach(function (segment) {
+              const displayArtist = segment.substring(segment.lastIndexOf('<') + 1, segment.lastIndexOf('>'));
+              displayText = segment.substring(segment.lastIndexOf('[') + 1, segment.lastIndexOf(']')) + '<br/>';
+              linkTarget = segment.substring(segment.lastIndexOf('(') + 1, segment.lastIndexOf(')'));
+              linkThumb = segment.substring(segment.lastIndexOf('{') + 1, segment.lastIndexOf('}'));
+
+              // Build elements
+              const eachLink = document.createElement('a');
+              const imgBox = document.createElement('div');
+              const imgElement = document.createElement('img');
+              const capArtist = document.createElement('h3');
+              const capText = document.createElement('h3');
+              const textContainer = document.createElement('div');
+
               imgBox.className = 'related-img';
               capArtist.className = 'related-artist';
               capText.className = 'related-title';
               textContainer.className = 'item-related-container';
-              let displayArtist = segment.substring(segment.lastIndexOf('<') + 1, segment.lastIndexOf('>'));
-              displayText = segment.substring(segment.lastIndexOf('[') + 1, segment.lastIndexOf(']')) + '<br/>';
-              linkTarget = segment.substring(segment.lastIndexOf('(') + 1, segment.lastIndexOf(')'));
-              linkThumb = segment.substring(segment.lastIndexOf('{') + 1, segment.lastIndexOf('}'));
+
               eachLink.href = linkTarget;
+              eachLink.target = target;
               imgElement.src = linkThumb;
-              eachLink.appendChild(imgBox);
               imgBox.appendChild(imgElement);
+              eachLink.appendChild(imgBox);
+
               capArtist.innerHTML = displayArtist;
               capText.innerHTML = `<em>${displayText}</em>`;
               textContainer.appendChild(capArtist);
               textContainer.appendChild(capText);
               eachLink.appendChild(textContainer);
+
               field.appendChild(eachLink);
             });
-          }
-          break;
-      }
-    })
+            break;
+        }
+      });
+    }
   }
-}
 
-let globalScope = true;
-let collectionScope = [
-    ''
-];
+  function applyAllReformats() {
+    reformatField('relate', 'markdown', '_self');
+    reformatField('relata', 'markdown', '_self');
+    reformatField('exhibd', 'markdown', '_self');
+  }
 
+  function waitForFieldAndApply(nickname, tries = 30) {
+    const interval = setInterval(() => {
+      const target = document.querySelector('tr.ItemMetadata-metadatarow.field-' + nickname + ' > td.field-value > span');
+      if (target || tries <= 0) {
+        clearInterval(interval);
+        applyAllReformats();
+      }
+      tries--;
+    }, 200); // Try every 200ms
+  }
 
-document.addEventListener('cdm-item-page:ready', function(e){
-    let item = e.detail.itemId;
-    let collection = e.detail.collectionId;
-    if (globalScope || collectionScope.includes(collection)) {
-      reformatField('relate','markdown');
-      reformatField('relata','markdown');
+  document.addEventListener('cdm-item-page:ready', function (e) {
+    if (e.detail.collectionId === 'leirner') {
+      waitForFieldAndApply('relate');
     }
-});
+  });
 
-document.addEventListener('cdm-item-page:update', function(e){
-    let item = e.detail.itemId;
-    let collection = e.detail.collectionId;
-    if (globalScope || collectionScope.includes(collection)) {
-      reformatField('relate','markdown');
-      reformatField('relata','markdown');
+  document.addEventListener('cdm-item-page:update', function (e) {
+    if (e.detail.collectionId === 'leirner') {
+      waitForFieldAndApply('relate');
     }
-});
-
-document.addEventListener('cdm-item-page:leave', function(e){
-    let collection = e.detail.collectionId;
-    if (globalScope || collectionScope.includes(collection)) {
-      //no clean up needed because metadata fields are always re-rendered
-    }
-});
+  });
 
 })();
-
---
+++ js/not-in-use/link-reformatter-thumb+artist_og.js
@@ -0,0 +1,80 @@
+(function () {
+  'use strict';
+function reformatField(nickname,syntax,target) {
+  let fieldsToChange = document.querySelectorAll('tr.ItemMetadata-metadatarow.field-' + nickname + ' > td.field-value > span');
+  if (fieldsToChange) {
+    fieldsToChange.forEach(function(field){
+      var originalText = field.textContent;
+      let displayText = '', linkTarget = '', linkThumb = '', origTextArray = [];
+      switch (syntax) {
+        case 'markdown':
+          origTextArray = originalText.split(';');
+          if (origTextArray) {
+            field.innerHTML = '';
+            origTextArray.forEach(function(segment){
+              let eachLink = document.createElement('a');
+              let imgBox = document.createElement('div');
+              let imgElement = document.createElement('img');
+              let capArtist = document.createElement('h3');
+              let capText = document.createElement('h3');
+              let textContainer = document.createElement('div');
+              imgBox.className = 'related-img';
+              capArtist.className = 'related-artist';
+              capText.className = 'related-title';
+              textContainer.className = 'item-related-container';
+              let displayArtist = segment.substring(segment.lastIndexOf('<') + 1, segment.lastIndexOf('>'));
+              displayText = segment.substring(segment.lastIndexOf('[') + 1, segment.lastIndexOf(']')) + '<br/>';
+              linkTarget = segment.substring(segment.lastIndexOf('(') + 1, segment.lastIndexOf(')'));
+              linkThumb = segment.substring(segment.lastIndexOf('{') + 1, segment.lastIndexOf('}'));
+              eachLink.href = linkTarget;
+              eachLink.target = target;
+              imgElement.src = linkThumb;
+              eachLink.appendChild(imgBox);
+              imgBox.appendChild(imgElement);
+              capArtist.innerHTML = displayArtist;
+              capText.innerHTML = `<em>${displayText}</em>`;
+              textContainer.appendChild(capArtist);
+              textContainer.appendChild(capText);
+              eachLink.appendChild(textContainer);
+              field.appendChild(eachLink);
+            });
+          }
+          break;
+      }
+    })
+  }
+}
+
+let globalScope = true;
+let collectionScope = [''];
+
+
+document.addEventListener('cdm-item-page:ready', function(e){
+    let item = e.detail.itemId;
+    let collection = e.detail.collectionId;
+    if (globalScope || collectionScope.includes(collection)) {
+      reformatField('relate','markdown','_self');
+      reformatField('relata','markdown','_self');
+      reformatField('exhibd','markdown','_self');
+    }
+});
+
+document.addEventListener('cdm-item-page:update', function(e){
+    let item = e.detail.itemId;
+    let collection = e.detail.collectionId;
+    if (globalScope || collectionScope.includes(collection)) {
+      reformatField('relate','markdown','_self');
+      reformatField('relata','markdown','_self');
+      reformatField('exhibd','markdown','_self');
+    }
+});
+
+document.addEventListener('cdm-item-page:leave', function(e){
+    let collection = e.detail.collectionId;
+    if (globalScope || collectionScope.includes(collection)) {
+      //no clean up needed because metadata fields are always re-rendered
+    }
+});
+
+})();
+
