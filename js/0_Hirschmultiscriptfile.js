e9baccc9fca68f5568b4aaafeef80769f1360487
Salongie <sarah.anne.long@gmail.com>
2025-10-09 15:25:32 -0700

    added js

--- js/0_Hirschmultiscriptfile.js
+++ js/0_Hirschmultiscriptfile.js
@@ -36,7 +36,9 @@ const scriptFilesToLoad = [
   'link-reformatter-more2.js',
   'link-reformatter-thumb+artist.js',
   'panel-expand.js',
+  'physogs.js',
   'pdf-objects-multipage-1_1.js',
+  'scrollbar.js',
 	'uv-cp.js',
 ];
 
